124432d9b57c56e91c55ce1da64cb35b2b154454
Ruberald <sajidhossain909@gmail.com>
2023-07-27 09:50:10 +0530

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,15 @@
+require("heinrichskies.remap")
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.scrolloff = 8
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.cmd([[highlight Pmenu ctermbg=white]])
